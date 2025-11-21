1cf131fb9a0b7e3c4914b3b26cf2162a0e5030e3
Deepayan0000 <145019195+Deepayan0000@users.noreply.github.com>
2025-11-21 16:27:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Show a welcome message when the page loads
-window.onload = function () {
-    console.log("Website loaded successfully!");
+function showInfo(message) {
+    alert(message);
+}
 
-    alert("Welcome to the Tourist Places Showcase!");
-};
-
-// Adds a small hover effect log for learning
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-    card.addEventListener("mouseenter", () => {
-        console.log("You hovered on:", card.querySelector("h2").innerText);
-    });
-});
